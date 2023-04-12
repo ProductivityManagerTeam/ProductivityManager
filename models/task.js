83b132c7ee8139ea507219b8fda5c0b79afc4d19
Manuel Becerra <manuelbecerra@Manuels-MacBook-Pro-2.local>
2023-04-11 21:42:08 -0400

    create task object and create get task list endpoint

--- models/task.js
+++ models/task.js
@@ -24,4 +24,4 @@ const taskSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('tasks', taskSchema);
+module.exports = mongoose.model('Task', taskSchema);
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Task = require('../models/task');
 mongoose.connect(process.env.DATABASE_URL);
+
+router.get('/task-list', getTasks, (req, res) => {
+    
+        res.json(res.taskList);
+
+});
+
+async function getTasks(req, res, next) {
+    let tasklist;
+
+    try {
+
+        taskList = await Task.find({userID: req.body.userID, currentDate: req.body.currentDate});
+
+        if (taskList == null) {
+
+            res.status(404).json({message: "Not Found"});
+
+        }
+
+    } catch (error) {
+
+        res.status(500).json({messag: error.message});
+    
+    }
+
+    res.taskList = taskList;
+    next();
+}
 
 module.exports = router;
