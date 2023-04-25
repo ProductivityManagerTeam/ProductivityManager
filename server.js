29d7227b1e4a17dc55b55f983b6de0583f4b5953
Manny2becerra <manny2becerra@gmail.com>
2023-04-24 23:00:21 -0400

    Deployment commits

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require("express");
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 const app = express();
+app.set('port', (process.env.PORT || 3000));
 const mongoose = require('mongoose');
 const database = 'PRODUCTIVITY_MANAGER';
 const collection = 'USERS';
@@ -16,4 +19,4 @@ app.use('/users', usersRouter);
 app.use('/tasks',tasksRouter);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on... ${port}`));
+app.listen(PORT, () => console.log(`Listening on... ${PORT}`));
